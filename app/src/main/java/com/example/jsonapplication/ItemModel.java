13982ca08b7337d374b9f4ea4fc191a89e7d6a12
PnkRabbit6322 <phungvu700@yahoo.com>
2022-01-01 02:02:05 +0700

    Refactor

--- app/src/main/java/com/example/jsonapplication/ItemModel.java
+++ app/src/main/java/com/example/jsonapplication/ItemModel.java
@@ -1,11 +1,123 @@
 package com.example.jsonapplication;
 
-public class ItemModel {
+import java.io.Serializable;
+
+public class ItemModel implements Serializable {
     private int id;
     private String name;
     private String userName;
-    private String Email;
+    private String email;
     private String thumbnailSource;
+    private String photoSource;
+    private String street;
+    private String suite;
+    private String city;
+    private String zipcode;
+    private String lat;
+    private String lng;
+    private String phone;
+    private String companyName;
+    private String catchPhrase;
+    private String bs;
+
+    public ItemModel(int id, String name, String userName, String email, String thumbnailSource, String photoSource, String street, String suite, String city, String zipcode, String lat, String lng, String phone, String companyName, String catchPhrase, String bs) {
+        this.id = id;
+        this.name = name;
+        this.userName = userName;
+        this.email = email;
+        this.thumbnailSource = thumbnailSource;
+        this.photoSource = photoSource;
+        this.street = street;
+        this.suite = suite;
+        this.city = city;
+        this.zipcode = zipcode;
+        this.lat = lat;
+        this.lng = lng;
+        this.phone = phone;
+        this.companyName = companyName;
+        this.catchPhrase = catchPhrase;
+        this.bs = bs;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getSuite() {
+        return suite;
+    }
+
+    public void setSuite(String suite) {
+        this.suite = suite;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCatchPhrase() {
+        return catchPhrase;
+    }
+
+    public void setCatchPhrase(String catchPhrase) {
+        this.catchPhrase = catchPhrase;
+    }
+
+    public String getBs() {
+        return bs;
+    }
+
+    public void setBs(String bs) {
+        this.bs = bs;
+    }
 
     public int getId() {
         return id;
@@ -32,11 +144,11 @@ public class ItemModel {
     }
 
     public String getEmail() {
-        return Email;
+        return email;
     }
 
     public void setEmail(String email) {
-        Email = email;
+        email = email;
     }
 
     public String getThumbnailSource() {
@@ -47,11 +159,11 @@ public class ItemModel {
         this.thumbnailSource = thumbnailSource;
     }
 
-    public ItemModel(int id, String name, String userName, String email, String thumbnailSource) {
-        this.id = id;
-        this.name = name;
-        this.userName = userName;
-        this.Email = email;
-        this.thumbnailSource = thumbnailSource;
+    public String getPhotoSource() {
+        return photoSource;
+    }
+
+    public void setPhotoSource(String photoSource) {
+        this.photoSource = photoSource;
     }
 }
--- app/src/main/java/com/example/jsonapplication/JSONDisplay.java
+++ app/src/main/java/com/example/jsonapplication/JSONDisplay.java
@@ -1,15 +1,45 @@
 package com.example.jsonapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 public class JSONDisplay extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jsondisplay);
+
+        ItemModel item = (ItemModel) getIntent().getSerializableExtra("item");
+
+        ImageView photo = findViewById(R.id.detail_photo);
+        TextView name = findViewById(R.id.detail_name);
+        TextView username = findViewById(R.id.detail_username);
+        TextView email = findViewById(R.id.detail_email);
+        TextView address = findViewById(R.id.detail_address);
+        TextView phone = findViewById(R.id.detail_phone);
+        TextView company = findViewById(R.id.detail_company);
+
+        Glide.with(this).load("https://lebavui.github.io" + item.getPhotoSource()).into(photo);
+        name.setText(item.getName());
+        username.setText(item.getUserName());
+        email.setText(item.getEmail());
+        address.setText("Address: " + item.getStreet() + ", " + item.getSuite() + ", "
+                + item.getCity() + ", " + item.getZipcode() + "\n" +
+                "Geo: " + item.getLat() + ", " + item.getLng());
+        phone.setText("Phone: " + item.getPhone());
+        company.setText("Company: " + item.getCompanyName() + "\n" + item.getCatchPhrase() + "\n" + item.getBs());
+
     }
 }
